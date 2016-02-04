8bb442af2bb67c2837785d10179ae0813432fda8
Kazato Sugimoto <uiureo@gmail.com>
2016-02-04 16:08:13 +0900

    Move ram into ram/

--- test.js
+++ test.js
@@ -2,7 +2,7 @@ const test = require('ava')
 const glob = require('glob')
 const exec = require('child_process').exec
 
-glob.sync('*.tst').forEach(file => {
+glob.sync('{,ram/}*.tst').forEach(file => {
   test.cb(file, t => {
     exec(`~/Downloads/nand2tetris/tools/HardwareSimulator.sh ${file}`, t.end)
   })
